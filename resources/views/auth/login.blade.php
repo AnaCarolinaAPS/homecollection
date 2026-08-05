3c83041ecf48e29d0281d4e768c19d5151ef732d
Ana Carolina dos Anjos <anacarolinaaps@gmail.com>
2026-08-05 14:35:51 -0300

    [layout] Auth Login ajuste de layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,61 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+@extends('auth.master')
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+@section('title')
+    <title>Login | HomeController</title>
+@endsection
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+@section('auth')
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+    <h4 class="text-muted text-center font-size-18"><b>Sign In</b></h4>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+    <div class="p-3">
+        <!-- Session Status -->
+        <x-auth-session-status class="mb-4" :status="session('status')" />
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+        <!-- Validation Errors -->
+        {{-- <x-auth-validation-errors class="mb-4" :errors="$errors" /> --}}
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
+        <form method="POST" action="{{ route('login') }}" class="form-horizontal mt-3">
+            @csrf
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
+            <div class="form-group mb-3 row">
+                <div class="col-12">
+                    <input class="form-control" type="text" required="" placeholder="Email" id="email" name="email" :value="old('email')" autofocus>
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                </div>
+            </div>
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+            <div class="form-group mb-3 row">
+                <div class="col-12">
+                    <input class="form-control" type="password" required="" placeholder="Password" id="password" name="password" autocomplete="current-password">
+                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                </div>
+            </div>
+
+            <div class="form-group mb-3 row">
+                <div class="col-12">
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="remember_me" name="remember">
+                        <label class="form-label ms-1" for="remember_me">Remember me</label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="form-group mb-3 text-center row mt-3 pt-1">
+                <div class="col-12">
+                    <button class="btn btn-info w-100 waves-effect waves-light" type="submit">Log In</button>
+                </div>
+            </div>
+
+            <div class="form-group mb-0 row mt-2">
+                <div class="col-sm-7 mt-3">
+                    <a href="{{ route('password.request') }}" class="text-muted"><i class="mdi mdi-lock"></i> Forgot your password?</a>
+                </div>
+                <div class="col-sm-5 mt-3">
+                    <a href="{{ route('register') }}" class="text-muted"><i class="mdi mdi-account-circle"></i> Create an account</a>
+                </div>
+            </div>
+        </form>
+    </div>
+    <!-- end -->
+@endsection
